cd19864098cae62494de292e7a8d3af9e420fd8e
Gabriel JUCHAULT <gabriel.juchault@gmail.com>
2018-06-11 10:20:13 +0200

    feat: add hero image

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -51,16 +51,9 @@ class Layout extends React.Component {
           toggleDrawer={this.toggleDrawer}
         />
         <Drawer opened={this.state.drawer} toggle={this.toggleDrawer} />
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
-        >
+        <main>
           {this.props.children()}
-        </div>
+        </main>
       </div>
     )
   }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import Link from 'gatsby-link'
+
+import hero from '../assets/hero.png'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <img src={hero} alt="RebirtDB hero" />
   </div>
 )
 
